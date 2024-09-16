6ad3c330853241fac9ed67f068f52caaa2e51edf
Zack Roulier <zroulier1@babson.edu>
2024-09-15 20:55:23 -0400

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Derive. Business Statistics Solutions</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+        return(
+            <header>
+                <h1>My Website</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li>Services</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                <hr></hr>
+            </header>
+        );
+}
+
+export default Header
